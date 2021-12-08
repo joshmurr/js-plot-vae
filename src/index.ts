b7175591ce12d0a20a7933db957d1b6cfed5e72f
Josh Murr <joshuadavidmurr@gmail.com>
2021-12-08 14:55:49 +0000

    Latents are back

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,28 @@
 import { vec3, mat4 } from 'gl-matrix'
 
-//import Renderer from './renderer'
 import GL_Handler from './gl_handler'
-import { pointsVert, pointsFrag } from './shader_programs/basic'
-//import LatentPoints from './latent_points'
-import RandomPointSphere from './random_point_sphere'
-//import NP_Loader from './npy_loader'
+//import { pointsVert, pointsFrag } from './shader_programs/basic'
+import { latentPointsVert, latentPointsFrag } from './shader_programs/basic'
+import LatentPoints from './latent_points'
+//import RandomPointSphere from './random_point_sphere'
+import NP_Loader from './npy_loader'
 
-//import all_z_mean from './assets/all_z_mean.npy'
-//import all_labels from './assets/all_train_labels.npy'
+import all_z_mean from './assets/all_z_mean.npy'
+import all_labels from './assets/all_train_labels.npy'
 
-//const R = new Renderer()
-//R.init(512, 512)
-
-//R.initShaderProgram('points', latentPointsVert, latentPointsFrag, 'POINTS')
-//R.initProgramUniforms('points', [
-//'u_ProjectionMatrix',
-//'u_ViewMatrix',
-//'u_Mouse',
-//])
-//R.initGeometryUniforms('points', ['u_ModelMatrix'])
-//R.cameraPosition = [0, 0, 0.2]
-
-//const n = new NP_Loader()
-//n.load(all_z_mean).then((latent_vals) => {
-//n.load(all_labels).then((labels) => {
-//const zs = new LatentPoints(R.gl, latent_vals, labels)
-//zs.rotate = { speed: 0.001, axis: [0, 1, 0] }
-//R.addGeometry('points', zs)
-
-//function draw(now: number) {
-//R.draw(now)
-//window.requestAnimationFrame(draw)
-//}
-//window.requestAnimationFrame(draw)
-//})
-//})
-
-// --------------------------
 type UniformDescs = {
   [key: string]: number | number[] | mat4 | vec3
 }
 
+const n = new NP_Loader()
+
 const G = new GL_Handler()
-const canvas = G.canvas(512, 512)
+const canvas = G.canvas(1024, 1024)
 const gl = G.gl
-const program = G.shaderProgram(pointsVert, pointsFrag)
+const program = G.shaderProgram(latentPointsVert, latentPointsFrag)
 gl.useProgram(program)
 
-const randomPS = new RandomPointSphere(gl, 1000)
-randomPS.rotate = { speed: 0.001, axis: [0, 1, 0] }
-
-randomPS.linkProgram(program)
-
-const cameraPos = vec3.fromValues(0, 0, 4)
+const cameraPos = vec3.fromValues(0, 0, 0.2)
 const up = vec3.fromValues(0, 1, 0)
 const target = vec3.fromValues(0, 0, 0)
 const viewMatrix = mat4.lookAt(mat4.create(), cameraPos, target, up)
@@ -74,32 +43,41 @@ const uniforms: UniformDescs = {
   u_ModelMatrix: mat4.create(),
   u_ViewMatrix: viewMatrix,
   u_ProjectionMatrix: projMatrix,
+  u_pointSize: 0.6,
 }
 
 const uniformSetters = G.getUniformSetters(program)
 
 G.setUniforms(uniformSetters, uniforms)
 
-function draw(time: number) {
-  gl.viewport(0, 0, canvas.width, canvas.height)
-  gl.clearColor(0, 0, 0, 0)
-  gl.clear(gl.COLOR_BUFFER_BIT)
-  gl.clearDepth(1.0)
-  gl.enable(gl.CULL_FACE)
-  gl.enable(gl.DEPTH_TEST)
+n.load(all_z_mean).then((latent_vals) => {
+  n.load(all_labels).then((labels) => {
+    const latents = new LatentPoints(gl, latent_vals, labels)
+    latents.linkProgram(program)
+    latents.rotate = { speed: 0.001, axis: [0, 1, 0] }
 
-  const modelMat = randomPS.updateModelMatrix(time)
+    function draw(time: number) {
+      gl.viewport(0, 0, canvas.width, canvas.height)
+      gl.clearColor(0.8, 0.8, 0.8, 1)
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+      gl.clearDepth(1.0)
+      gl.enable(gl.CULL_FACE)
+      gl.enable(gl.DEPTH_TEST)
 
-  G.setUniforms(uniformSetters, { u_ModelMatrix: modelMat })
+      const modelMat = latents.updateModelMatrix(time)
 
-  gl.bindVertexArray(randomPS.VAO)
+      G.setUniforms(uniformSetters, { u_ModelMatrix: modelMat })
 
-  gl.drawArrays(gl.POINTS, 0, randomPS.numVertices)
+      gl.bindVertexArray(latents.VAO)
 
-  gl.bindVertexArray(null)
-  gl.bindBuffer(gl.ARRAY_BUFFER, null)
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null)
+      gl.drawArrays(gl.POINTS, 0, latents.numVertices)
 
-  requestAnimationFrame(draw)
-}
-draw(0)
+      gl.bindVertexArray(null)
+      gl.bindBuffer(gl.ARRAY_BUFFER, null)
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null)
+
+      requestAnimationFrame(draw)
+    }
+    draw(0)
+  })
+})
--- src/shader_programs/basic.ts
+++ src/shader_programs/basic.ts
@@ -47,11 +47,12 @@ const latentPointsFrag = `#version 300 es
 precision mediump float;
 
 in vec3 v_Color;
+uniform float u_pointSize;
 out vec4 OUTCOLOUR;
 
 void main(){
     float distance = length(2.0 * gl_PointCoord - 1.0);
-    if (distance > 0.3) {
+    if (distance > u_pointSize) {
             discard;
     }
     OUTCOLOUR = vec4(v_Color, 1.0);
