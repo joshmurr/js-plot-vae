410310e4b5c5dcadbf8e1493c57df8746dbf5d25
Josh Murr <joshuadavidmurr@gmail.com>
2021-12-16 23:55:12 +0000

    Remove reparameterise

--- src/vae.ts
+++ src/vae.ts
@@ -4,8 +4,7 @@ import NN from './neural_net'
 
 interface Config {
   path: string
-  mean?: string
-  log_var?: string
+  z?: string
   labels?: string
   width?: number
   height?: number
@@ -27,9 +26,10 @@ export default class VAE extends Model {
     ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
   }
 
-  public async run(mean: Float32Array, logvar: Float32Array) {
+  public async run(mean: Float32Array) {
     const logits = tf.tidy(() => {
-      const z = this.reparameterize(mean, logvar).expandDims(0)
+      //const z = this.reparameterize(mean, logvar).expandDims(0)
+      const z = tf.tensor([mean])
       const decoded = this.model.predict(z) as tf.Tensor
       return decoded.sigmoid()
     })
@@ -39,40 +39,22 @@ export default class VAE extends Model {
     return d
   }
 
-  private reparameterize(mean: Float32Array, logvar: Float32Array) {
-    const m = tf.tensor(mean)
-    const l = tf.tensor(logvar)
-    const exp = tf.exp(l.mul(0.5)).add(m)
-
-    return exp
-  }
-
-  private reparameterize_with_noise(mean: Float32Array, logvar: Float32Array) {
-    const m = tf.tensor(mean)
-    const l = tf.tensor(logvar)
-    const exp = tf.exp(l.mul(0.5))
-    const eps = tf.randomNormal(m.shape)
-    const ret = eps.mul(exp).add(m)
-
-    return ret
-  }
-
   public dispose() {
     this.model.dispose()
     tf.disposeVariables()
   }
 
-  private async generateImageTensors(points: number[], mean_to_log: NN) {
+  private async generateImageTensors(points: number[]) {
     const output_tensors: Array<tf.Tensor2D> = []
     for (let i = 0; i < points.length; i += 3) {
       const mean = points.slice(i, i + 3)
       const mean_t = tf.tensor([[...mean]])
-      const log_var = await mean_to_log.run(mean_t)
-      const log_var_d = await log_var.data()
+      //const log_var = await mean_to_log.run(mean_t)
+      //const log_var_d = await log_var.data()
 
       const output = await this.run(
-        new Float32Array(mean),
-        log_var_d as Float32Array
+        new Float32Array(mean)
+        //log_var_d as Float32Array
       )
 
       output_tensors.push(output as tf.Tensor2D)
@@ -80,8 +62,8 @@ export default class VAE extends Model {
     return output_tensors
   }
 
-  public async latentTraversal(points: number[], mean_to_log: NN) {
-    const image_tensors = await this.generateImageTensors(points, mean_to_log)
+  public async latentTraversal(points: number[]) {
+    const image_tensors = await this.generateImageTensors(points)
 
     image_tensors.forEach((t: tf.Tensor2D) => {
       const canvas = document.createElement('canvas')
