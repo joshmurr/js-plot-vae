4f9c19af68b9a1674c6e88dafdd729b99e917ddb
Josh Murr <joshuadavidmurr@gmail.com>
2021-12-17 21:13:01 +0000

    Class labels

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,9 @@ type AllConfigs = { [key: string]: Config }
 
 interface Config {
   path: string
-  mean?: string
-  log_var?: string
   z?: string
   labels?: string
+  class_labels?: Array<number | string>
   width?: number
   height?: number
   input_shape: number[]
@@ -16,6 +15,7 @@ export const config: AllConfigs = {
     path: './assets/vae_decoder_reparam/model.json',
     z: './assets/model_data/mnist/all_z_mnist.npy',
     labels: './assets/model_data/mnist/all_train_labels_mnist.npy',
+    class_labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
     width: 28,
     height: 28,
     input_shape: [1, 3],
@@ -24,6 +24,18 @@ export const config: AllConfigs = {
     path: './assets/vae_fashion_reparam/model.json',
     z: './assets/model_data/fashion/all_z_fashion_mnist.npy',
     labels: './assets/model_data/fashion/all_train_labels_fashion_mnist.npy',
+    class_labels: [
+      'T-shirt/top',
+      'Trouser',
+      'Pullover',
+      'Dress',
+      'Coat',
+      'Sandal',
+      'Shirt',
+      'Sneaker',
+      'Bag',
+      'Ankle boot',
+    ],
     width: 28,
     height: 28,
     input_shape: [1, 3],
--- src/vae.ts
+++ src/vae.ts
@@ -5,6 +5,7 @@ interface Config {
   path: string
   z?: string
   labels?: string
+  class_labels?: Array<number | string>
   width?: number
   height?: number
   input_shape: number[]
