777f996888a3d825c2f90d97d608127c7c69fe8b
Josh Murr <joshuadavidmurr@gmail.com>
2021-12-22 13:14:16 +0000

    Styling.. ish

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Interacting with the Latent Space of a VAE
 
-![Output](./output_171221.png)
+![Output](./output_221221.png)
--- src/index.ts
+++ src/index.ts
@@ -250,9 +250,9 @@ function main(model_name: string) {
     z_points.pallette.map((rgba, i) => {
       const li = document.createElement('li')
       li.innerText = `${config[model_name].class_labels[i]}`
-      li.addEventListener('click', () => z_points.selectClassLabel(i))
       const [r, g, b] = rgba
       li.style.background = `rgb(${r * 255},${g * 255},${b * 255})`
+      li.addEventListener('click', () => z_points.selectClassLabel(i))
       pallette_el.appendChild(li)
     })
 
